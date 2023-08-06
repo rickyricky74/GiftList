e382dd384d82c17d41f609781250da67767f1e76
Ricardo Pena <ricardopena@email.com>
2023-08-06 15:01:52 -0500

    Finished implementation

--- client/index.js
+++ client/index.js
@@ -5,10 +5,15 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+
+  const name = process.argv[2] ? process.argv[2] : '';
+
+  const m = new MerkleTree(niceList);
+  const proof = m.getProof(niceList.findIndex(n => n === name));
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof: proof,
+    name: name,
   });
 
   console.log({ gift });
